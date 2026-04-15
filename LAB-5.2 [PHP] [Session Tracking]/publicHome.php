ea6f43676dac40e7f918755802949110c6144eb3
Abdullah-Al-Amin <inabdullahalamin@gmail.com>
2026-04-15 13:26:20 +0600

    1st Commit

---
+++ LAB-5.2 [PHP] [Session Tracking]/publicHome.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Public Home</title>
+
+    
+</head>
+<body>
+    <div class="container">
+    <div class="header">
+        <img src="logo.png" alt="XCompany Logo">
+        
+        <div class="nav">
+            <a href="publicHome.php">Home</a> |
+            <a href="login.php">Login</a> |
+            <a href="registration.php">Registration</a>
+        </div>
+    </div>
+
+    
+    <div class="content">
+        <h3>Welcome to XCompany</h3>
+    </div>
+
+    <div class="footer">
+        Copyright © 2017
+    </div>
+
+</div>
+</body>
+</html>
