8e7f09535f615c6bbb8bb4ed5593f3095cdec37e
Abdullah-Al-Amin <inabdullahalamin@gmail.com>
2026-04-15 13:56:03 +0600

    3rd Commit

--- LAB-5.2 [PHP] [Session Tracking]/registration.php
+++ LAB-5.2 [PHP] [Session Tracking]/registration.php
@@ -43,10 +43,30 @@
         <form action="post" enctype="multipart/form-data">
             <fieldset>
                 <legend>REGISTRATION</legend>
-                <label for="">Name</label>
-                <input type="text" name="username"><hr>
-                <label for="">Email</label>
-                <input type="email" name="email"><hr>
+                Name: <input type="text" name="name"><hr>
+                Email: <input type="email" name="email"><hr>
+                Username: <input type="text" name="username"><hr>
+                Password: <input type="password" name="password"><hr>
+                Confirm Password: <input type="password" name="cpassword"><hr>
+                <fieldset>
+                    <legend>Gender</legend>
+                    <input type="radio" name="gender" value="Male">Male
+                <input type="radio" name="gender" value="Female">Female<hr>
+                </fieldset>
+                
+                <fieldset>
+                    <legend>DOB</legend>
+                    <input type="text" size="2" maxlength="2" name="" value=""/> / 
+                <input type="text" size="2" maxlength="2" name="" value=""/> / 
+                <input type="text" size="4" maxlength="4" name="" value=""/> 
+                (dd/mm/yyyy)  
+                </fieldset>
+
+                
+
+                <input type="submit" name="submit" value="Submit">
+                <input type="reset" name="reset" value="Reset">
+
             </fieldset>
 
         </form>
